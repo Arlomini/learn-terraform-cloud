9f097d80dc24778ce2288d2bb306f076bd495b0f
ARMAND LOMINI <107315783+Arlomini@users.noreply.github.com>
2024-12-12 06:32:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,4 +12,7 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "my-ec2-cloud"
 }
-
+variable "subnet_id" {
+  type = string
+  default = subnet-04e67d2d384b9e596
+}
